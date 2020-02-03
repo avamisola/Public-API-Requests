0acf114bd27ccf0407c0f07d7bc22dcf6c1c03e4
avamisola <58097632+avamisola@users.noreply.github.com>
2020-02-02 22:08:03 -0500

    prepping modal update
    -prepping modal update
    -added semicolons

--- js/script.js
+++ js/script.js
@@ -3,42 +3,51 @@ Treehouse Full Stack JavaScript Techdegree
 Project 5 - Public API Requests
 *****************************************/
 
-let thumbnail
-const ajaxParam = '?results=12&inc=picture,name,email,cell,location,dob'
-
+//
+const ajaxParam = '?results=12&inc=picture,name,email,cell,location,dob&nat=us';
 
 //
 $.ajax({
   url: `https://randomuser.me/api/${ajaxParam}`,
   dataType: 'json',
   success: function(data) {
-    const result = data.results
+    const result = data.results;
     console.log(result);
     $.each(result, function(index) {
-      //const thumbnail = $(this).picture.thumbnail
-      //console.log(result[index].name);
-      const picture = result[index].picture.medium
-      const firstName = result[index].name.first
-      const lastName = result[index].name.last
-      const email = result[index].email
-      const city = result[index].location.city
-      const state = result[index].location.state
-      const galleryHTML =
-      `<div class="card">
-      <div class="card-img-container">
-          <img class="card-img" src=${picture} alt="profile picture">
-      </div>
-      <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-          <p class="card-text">${email}</p>
-          <p class="card-text cap">${city}, ${state}</p>
-      </div>
+      const picture = result[index].picture.large;
+      const firstName = result[index].name.first;
+      const lastName = result[index].name.last;
+      const email = result[index].email;
+      const city = result[index].location.city;
+      const state = result[index].location.state;
+      const cardHTML = `
+      <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${picture}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+            <p class="card-text">${email}</p>
+            <p class="card-text cap">${city}, ${state}</p>
+        </div>
       </div>`;
-      $('.gallery').append(galleryHTML);
-    })    
+      const modalHTML = `
+      <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${picture}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+                <p class="modal-text">${email}</p>
+                <p class="modal-text cap">city</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., ${city}, ${state} 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
+      </div>`;
+      $('.gallery').append(cardHTML);
+    });
   }
 });
-
-
-
-
